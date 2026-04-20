45823e34849d04dbfd8210e273a973297792bb53
tapan0810 <arceus0810@gmail.com>
2026-04-20 21:50:28 +0530

    feat : Implemented StudentUpdateDto with required Data Annotations

---
+++ ADO.NET_API_1/DTOs/StudentUpdateDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ADO.NET_API_1.DTOs
+{
+    public class StudentUpdateDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(255)]
+        public string Email { get; set; } = string.Empty;
+
+        [Range(1, 150)]
+        public int Age { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Department { get; set; } = string.Empty;
+    }
+}
