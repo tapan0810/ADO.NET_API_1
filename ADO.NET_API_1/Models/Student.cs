dca960ece1a16174d74b732a2256c23ec5c9e23a
tapan0810 <arceus0810@gmail.com>
2026-04-20 21:44:32 +0530

    feat : Implemented Model and Connection String

---
+++ ADO.NET_API_1/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace ADO.NET_API_1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string Department { get; set; } = string.Empty;
+    }
+}
